5165c6567bda375a6cd2d6d224ee0796e4c191c7
rslgp <rslgp@cin.ufpe.br>
2023-09-25 18:14:23 -0300

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function Header() {
+  return (
+    <div className="Header">
+        <div className='espacamento'/>
+        <h1>Bater ponto na ECS</h1>
+        <div className='espacamento'/>
+    </div>
+  );
+}
+
+export default Header;
