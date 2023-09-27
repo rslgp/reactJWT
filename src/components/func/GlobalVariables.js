60fc38b02b7b2b3da390881af88fd309c4acbbeb
rslgp <rslgp@cin.ufpe.br>
2023-09-27 13:27:47 +0000

    fix hashroute routes

--- src/components/func/GlobalVariables.js
+++ src/components/func/GlobalVariables.js
@@ -1,5 +1,5 @@
 const GlobalVariables = {}
-GlobalVariables.baseURL="/reactJWT";
-GlobalVariables.loginPage=GlobalVariables.baseURL;
+GlobalVariables.baseURL="reactJWT/#";
+GlobalVariables.loginPage=GlobalVariables.baseURL+"/auth/login";
 GlobalVariables.profilePage=GlobalVariables.baseURL+"/page";
 export default GlobalVariables;
